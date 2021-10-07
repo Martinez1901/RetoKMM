eaf830e7a810b206d5908c320ca06df1039dada1
Dumar Yeksel Basto Moreno - Ceiba Software <dumar.basto@ceiba.com.co>
2021-10-07 15:03:29 -0500

    Se ajusta ruta del Podfile

--- infrastructure/shared/build.gradle.kts
+++ infrastructure/shared/build.gradle.kts
@@ -26,7 +26,7 @@ kotlin {
         homepage = "Link to the Shared Module homepage"
         ios.deploymentTarget = "14.1"
         frameworkName = "shared"
-        podfile = project.file("../iosApp/Podfile")
+        podfile = project.file("../../iosApp/Podfile")
     }
     
     sourceSets {
